ffcf621609cee3683b068645f17de6fb8cad176d
Gevorg <margaryan.gevorg.25@gmail.com>
2026-08-01 13:11:00 +0400

    BuildingType modified

--- Revorq.DAL/Enums/BuildingType.cs
+++ Revorq.DAL/Enums/BuildingType.cs
@@ -5,8 +5,12 @@ public enum BuildingType
     Residential = 0,
     Commercial = 1,
     Industrial = 2,
-    Institutional = 3,
-    Specialty = 4,
+    Hotel = 3,
+    House = 4,
     Government = 5,
-    Other = 6
+    Cultural = 6,
+    Shop = 7,
+    Public = 8,
+    Cunstruction = 9,
+    Other = 10
 }
